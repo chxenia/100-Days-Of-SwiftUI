af0339cfb2aad76344efc0431e460b157feec0b4
Xenia <145117372+chxenia@users.noreply.github.com>
2023-09-18 17:22:01 +0300

    Add files via upload

---
+++ Day 20-22/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Xenia on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
