6f4638a4a8856583478c9643975dd1991ee64ff7
Xenia <145117372+chxenia@users.noreply.github.com>
2023-09-24 19:06:08 +0300

    Add files via upload

---
+++ Day 25/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Xenia on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
