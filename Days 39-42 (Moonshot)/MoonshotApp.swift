1b77ead46cab44300b033327b279a258170bb0d3
Xenia <145117372+chxenia@users.noreply.github.com>
2023-10-07 22:24:34 +0300

    Add files via upload

---
+++ Days 39-42 (Moonshot)/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Xenia on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
